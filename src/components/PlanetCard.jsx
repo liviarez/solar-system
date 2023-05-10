729bdb360fb0f5e1ab587c3b84577bba5ac2ab3a
liviarez <mrezende.livia@gmail.com>
2023-05-10 17:46:15 -0300

    Feat:9. Crie um componente chamado MissionCard.

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="card">
         <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
