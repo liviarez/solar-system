9e1bd68e0df679e1c58614cea1d40af1a534dde3
liviarez <mrezende.livia@gmail.com>
2023-10-02 17:06:36 -0300

    Feat: MissionCard Css

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Css/MissionCard.css';
 
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
+      <div data-testid="mission-card" className="mission-card">
+
+        <h2 data-testid="mission-name">{name}</h2>
+
+        <hr className="mission-line" />
+
+        <div className="mission-information">
+          <div className="year-country">
+            <p data-testid="mission-year" className="info-tag">{year}</p>
+            <p
+              data-testid="mission-country"
+              className="info-tag"
+            >
+              {country}
+
+            </p>
+          </div>
+          <div className="destination">
+            <p
+              data-testid="mission-destination"
+              className="destination-tag"
+            >
+              {destination}
+
+            </p>
+          </div>
+        </div>
       </div>
 
     );
